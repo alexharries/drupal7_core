db0b3498202112854c401f34b4b4997c3ef07be7
Alex Harries <alex@greyhead.co.uk>
2016-10-18 23:46:02 +0100

    Set port 3306 by default.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- configuration/database.settings.php
+++ configuration/database.settings.php
@@ -21,7 +21,7 @@ if (!(strtok($_SERVER['REQUEST_URI'],'?') == '/install.php') && (!isset($databas
             'username' => '',
             'password' => '',
             'host' => '127.0.0.1',
-            'port' => '',
+            'port' => '3306',
             'driver' => 'mysql',
             'prefix' => '',
           ),
